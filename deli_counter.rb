cad98ac284d45bf7e6c9abae717e1b6c628f03f3
Kevin Chan <kevchan111@gmail.com>
2018-04-10 21:08:17 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,9 +5,9 @@
    else
      output = "The line is currently:"
      katz_deli.each_with_index {|name , position|
-       output += "#{position+1}. #{name}"
+       output += " #{position+1}. #{name}"
      }
-     return output
+     puts "#{output}"
    end
 end
 
