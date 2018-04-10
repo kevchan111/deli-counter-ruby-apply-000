ddfae42880919ab0526e1097c92e74e3c6024faf
Kevin Chan <kevchan111@gmail.com>
2018-04-10 20:24:52 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+katz_deli = []
+
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+
+def line(arr)
+  if arr == []
+    puts "There line is empty"
+  else
+      output = "The line is currently: "
+      arr.each_with_index {|name,index| output += "#{index+1}. #{name} "}
+  end
+      puts output
+end
+
+
+def now_serving(arr)
+  removed = arr.shift
+  if arr == []
+    puts "There is currently no one in line"
+  else
+    puts "Currently Serving: #{removed}"
+end
+end
