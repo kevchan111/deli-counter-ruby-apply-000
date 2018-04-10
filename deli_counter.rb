cdb0464a5c96000fea2d8ae17c416bc5bd3d8bda
Kevin Chan <kevchan111@gmail.com>
2018-04-10 20:38:19 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,18 +1,18 @@
 katz_deli = []
 
-def line(katz_deli)  
-  if katz_deli.empty? 
+def line(katz_deli)
+  if katz_deli.empty?
     puts "The line is currently empty."
   else
-    puts "The line is currently: 1. #{katz_deli[0]} 2. #{katz_deli[1]} 3. #{katz_deli[2]}" 
+    puts "The line is currently: 1. #{katz_deli[0]} 2. #{katz_deli[1]} 3. #{katz_deli[2]}"
   end
 end
 
 def take_a_number(katz_deli,person)
-  if katz_deli.empty? 
+  if katz_deli.empty?
     katz_deli.push (person)
     puts "Welcome, #{person}. You are number 1 in line."
-  else 
+  else
     katz_deli.push (person)
     puts "Welcome, #{person}. You are number 4 in line."
   end
